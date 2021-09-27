4280ad605e79b6d7accedf1defa7bd190fb2470b
Gabriel Belo <gabrielbelo2007@gmail.com>
2021-09-27 20:09:29 -0300

    Organizando 20 a 29

---
+++ Aula 20 a 29/Aula 20/Aula 20.py
@@ -0,0 +1,25 @@
+#        Indices
+#        0123456789.......................33
+
+frase = 'O rato roeu a roupa do rei de roma'  # Valor iteravel
+tamanho_frase = len(frase)
+nova_string = ''
+contador = 0
+
+letra_maiscula = input('Qual letra deseja colocar maiscula:')
+
+# Iteração / Iterar (ato de percorrer cada elemento da sua str, ou do elemento iteravel)
+
+while contador < tamanho_frase:
+    # print(frase[contador], contador)
+    letra = frase[contador]
+
+    if letra == letra_maiscula:
+        nova_string += letra_maiscula.upper()
+
+    else:
+        nova_string += frase[contador]
+    contador += 1
+
+print(nova_string)
+print(frase)
