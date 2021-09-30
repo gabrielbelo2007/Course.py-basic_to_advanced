9d59c4f94179903650a86492cc6f57d5469dc92e
Gabriel Belo <gabrielbelo2007@gmail.com>
2021-09-30 18:15:08 -0300

    Mais completo

--- Aulas 10 a 19/Aula 17 - Índices e Fatiamentos (str)/Aula 17.py
+++ Aulas 10 a 19/Aula 17 - Índices e Fatiamentos (str)/Aula 17.py
@@ -12,17 +12,19 @@ texto =       'Python s2'
 # negativos  -[987654321]
 
 print(texto[2])
-print(texto[:-2])  # retira oq está no índice e tudo q vem dps dele
+print(texto[:-2])  # Retira o o índice e tudo que vem após ele
 
-nova_string = texto[2:6]  # fatiamento de str
-nova_string1 = texto[:6]  # sem número no 'inicio' ele começa do 0
-nova_string2 = texto[5:]  # sem número no 'fim' ele vai até o ultimo indice
-nova_string3 = texto[::2]  # o passo de leitura
+nova_string = texto[2:6]  # O número que determina a parada do fatiamento não conta
+nova_string1 = texto[:6]  # Sem número no 'inicio' ele começa do 0
+nova_string2 = texto[5:]  # Sem número no 'fim' ele vai até o ultimo indice
+nova_string3 = texto[::2]  # O passo de leitura
+nova_string4 = texto[::-1]  # Negativo além de alterar o passo inverte a ordem de leitura
 
 print(nova_string)
 print(nova_string1)
 print(nova_string2)
 print(nova_string3)
+print(nova_string4)
 
 for letra in texto:
     print(letra)
