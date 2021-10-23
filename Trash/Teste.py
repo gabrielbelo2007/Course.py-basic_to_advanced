1c2e7f8dd94ce926b696d4d3e118a93bddea7cf0
Gabriel Belo <gabrielbelo2007@gmail.com>
2021-10-23 13:30:29 -0300

    Funções Built-in + Revisões

--- Trash/Teste.py
+++ Trash/Teste.py
@@ -1,10 +1,2 @@
 
 
-nome_1 = 'Gabriel'
-sobrenome = 'Belo'
-nome_formatado = '{0} {1:#^50}'.format(nome_1, sobrenome)
-print(f'{nome_1:#^50}')
-print(nome_formatado)
-
-num_4 = 1
-print(f'{num_4:0>10}')  # a variavel tem que ter 10 casas contando com o número principal
