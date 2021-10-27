780e2752d77da2c90d9edd51fd29876dbd0ed75b
Gabriel Belo <gabrielbelo2007@gmail.com>
2021-10-27 15:46:43 -0300

    Desafio Validar CPF - Completo

--- Aulas 20 a 29/Aula 29 - Atividades/Desafio Validar CPF.py
+++ Aulas 20 a 29/Aula 29 - Atividades/Desafio Validar CPF.py
@@ -1,13 +1,15 @@
 
-cpf = [0, 2, 2, 7, 0, 7, 7, 9, 4, 6, 0]
+cpf = input('Qual é o seu CPF: ')
 novo_cpf = cpf[:-2]
 
 contador = 8
 numerador = 10
 valor_temp = 0
 total = 0
+validar = 0
 
 for valor in novo_cpf:
+    valor = int(valor)
     while contador < 9:
         valor_temp = valor * numerador
         contador += 1
@@ -19,17 +21,18 @@ else:
     validar = 11 - (total % 11)
     if validar > 9:
         validar = 0
-        novo_cpf.append(validar)
+        novo_cpf += str(validar)
 
     else:
         validar = validar
-        novo_cpf.append(validar)
+        novo_cpf += str(validar)
 
 total = 0
 numerador = 11
 valor_temp = 0
 
 for valor in novo_cpf:
+    valor = int(valor)
     while contador < 9:
         valor_temp = valor * numerador
         contador += 1
@@ -37,16 +40,15 @@ for valor in novo_cpf:
     contador = 8
     total += valor_temp
 
-
 else:
     validar = 11 - (total % 11)
     if validar > 9:
         validar = 0
-        novo_cpf.append(validar)
+        novo_cpf += str(validar)
 
     else:
         validar = validar
-        novo_cpf.append(validar)
+        novo_cpf += str(validar)
 
 if novo_cpf == cpf:
     print('O seu CPF é valido!')
