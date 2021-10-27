bdbb1cd5d4294f10af9b238d294ed9e4ec378377
Gabriel Belo <gabrielbelo2007@gmail.com>
2021-10-27 08:50:11 -0300

    Desafio Contadores

---
+++ Aulas 20 a 29/Aula 29 - Atividades/Desafio Contadores.py
@@ -0,0 +1,29 @@
+"""
+contador = [10]
+final = 0
+diminuir = 9
+
+for pr, re in enumerate(contador):
+    print(pr, re)
+    contador.append(diminuir)
+    diminuir -= 1
+    if diminuir == final:
+        break
+"""
+
+'''
+contador = 0
+numerador = 10
+
+while contador < 9:
+    print(contador, numerador)
+    contador += 1
+    numerador -= 1
+'''
+
+'''
+Maneira usada pelo professor:
+
+for p, r in enumerate(range(10, 1, -1)):
+    print(ind, r)
+'''
