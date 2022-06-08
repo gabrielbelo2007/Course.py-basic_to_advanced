6c43d0e178cd6f5a18055695fd1f6a861024139f
Gabriel Belo <91219977+gabrielbelo2007@users.noreply.github.com>
2022-06-08 18:35:09 -0300

    nothing

--- Python Intermediário/Aula 30 a 39/Aula 30/exer_funcoes_1_2.py
+++ Python Intermediário/Aula 30 a 39/Aula 30/exer_funcoes_1_2.py
@@ -54,9 +54,9 @@ número enviado.
 # def fb(n):
 #     if n % 3 == 0 and n % 5 == 0:
 #         return f'fizzbuzz, {n} é divisível por 3 e 5'
-#     if n % 5 == 0:
+#     elif n % 5 == 0:
 #         return f'buzz, {n} é divisível por 5'
-#     if n % 3 == 0:
+#     elif n % 3 == 0:
 #         return f'fizz, {n} é divisível por 3'
 #     return n
 #
