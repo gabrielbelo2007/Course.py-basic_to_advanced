8d92086506d19fa8b64fe3c85b51439bc1f80e18
Gabriel Belo <91219977+gabrielbelo2007@users.noreply.github.com>
2022-07-28 20:42:15 -0300

    Aula 33

---
+++ Python Intermediário/Aula 30 a 39/Aula 34/Aula 34 - Dicionários em python.py
@@ -0,0 +1,5 @@
+# Funciona como uma lista, porém não tem indices criados automaticamente
+# Voce organiza os indices nas funções e esses indices são chamados chaves
+# Um dicionário é uma estrutura de dados que suporta pares de chaves e valores
+
+
