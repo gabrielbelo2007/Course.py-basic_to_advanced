f78bc370cdee0060efddbcc1dd548ec67a62de23
Gabriel Belo <91219977+gabrielbelo2007@users.noreply.github.com>
2022-08-22 15:42:53 -0300

    Aula 37 e atividade - 68 (completa)

--- Python Intermediário/Aula 30 a 39/Atividades/Atividade - 68.py
+++ Python Intermediário/Aula 30 a 39/Atividades/Atividade - 68.py
@@ -1,6 +1,7 @@
 string = '01234567890123456789012345678901234567890123456789'
 
-l1 = [string.split("9", string.count('9') - 1)]
+n = 10
+l1 = [string[i:i+n] for i in range(0, len(string), n)]
 l2 = [string.replace('9', '9.', string.count('9') - 1)]
 print(l1)
 print(l2)
