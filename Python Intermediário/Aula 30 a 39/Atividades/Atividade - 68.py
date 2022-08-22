3bbd4f9b8af14c85ccb11b2f46b0d47752a17651
Gabriel Belo <91219977+gabrielbelo2007@users.noreply.github.com>
2022-08-22 15:45:52 -0300

    Aula 37 e atividade - 68 (completa)

--- Python Intermediário/Aula 30 a 39/Atividades/Atividade - 68.py
+++ Python Intermediário/Aula 30 a 39/Atividades/Atividade - 68.py
@@ -2,7 +2,8 @@ string = '01234567890123456789012345678901234567890123456789'
 
 n = 10
 l1 = [string[i:i+n] for i in range(0, len(string), n)]
-l2 = [string.replace('9', '9.', string.count('9') - 1)]
+retorno1 = [string.replace('9', '9.', string.count('9') - 1)]
+retorno2 = ".".join(l1)
 print(l1)
-print(l2)
-
+print(retorno1)
+print(retorno2)
