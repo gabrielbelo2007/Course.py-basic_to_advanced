6d47873023602a874e291d31733a4b5a0c0c6dec
Gabriel <gabrielbelo2007@gmail.com>
2024-05-16 14:01:20 -0300

    Fim - Basic Python

--- Python Básico/Aulas 20 a 29/Aula 28 - Split + Join + Strip [93]/Aula 28.py
+++ Python Básico/Aulas 20 a 29/Aula 28 - Split + Join + Strip [93]/Aula 28.py
@@ -3,7 +3,7 @@ split e join com Lista e str
 
 split - divide uma string
 join - une uma string
-strip - retira caracteres de uma string
+strip - retira caracteres de uma string do inicio e do final
 """
 
 frase = "     Olha que,     coisa interessante"
@@ -11,7 +11,7 @@ lista_frases_separadas = frase.split(",")  # Divide os valores da str divididos 
 
 lista_frases_juntas = []
 for i, frase in enumerate(lista_frases_separadas):
-    lista_frases_juntas.append(lista_frases_separadas[i].strip())  # Remove os espacos em branco 
+    lista_frases_juntas.append(lista_frases_separadas[i].strip())  # Remove os espacos em branco  
     
 print(lista_frases_separadas)
 print(lista_frases_juntas)
